71a1e625b943134f9eac24ca58be919ae8866e76
Femi Ibiwoye <femibiwoye@gmail.com>
2026-08-12 20:16:11 +0100

    Add OpenChest script to handle chest opening mechanics

---
+++ Assets/scripts/OpenChest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+    [SerializeField] float internalDistance;
+    [SerializeField] bool chestOpen;
+    [SerializeField] GameObject chest;
+    [SerializeField] float openDistance = 2f;
+
+    void Update()
+    {
+        internalDistance = RayCasting.distanceFromTarget;
+        if (chestOpen == false && internalDistance > 0 && internalDistance < openDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                chestOpen = true;
+                GameObject targetChest = chest != null ? chest : gameObject;
+                targetChest.GetComponent<Animator>().Play("Open chest");
+            }
+        }
+    }
+
+
+}
